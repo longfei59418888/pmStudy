dc4d73a39043d4b75d09406431b6d41deb62a7c2
longfei59418888 <398497684@qq.com>
2019-09-04 17:53:28 +0800

    undefined

--- webpack/main.js
+++ webpack/main.js
@@ -1,5 +1,8 @@
 
-const a = require('./m1')
-const b= require('./m2')
-// import { test } from './m1'
+const a = require('./components/a.vue')
+const b  = import('./m2')
+const d  = import('./m1')
+b.then((rst)=>{
+    console.log(rst)
+})
 console.log(a)
